d9a12d0bfeae82e786207d93657883c1111eea76
HyunjeJo <retriever89@gmail.com>
2013-06-10 22:36:54 +0900

    NetworkFileSystemClient 구현 Test 필요

--- src/main/java/org/herring/core/nfs/NetworkFileSystemClient.java
+++ src/main/java/org/herring/core/nfs/NetworkFileSystemClient.java
@@ -4,6 +4,9 @@ import org.herring.core.protocol.ClientComponent;
 import org.herring.core.protocol.codec.HerringCodec;
 import org.herring.core.protocol.codec.SerializableCodec;
 import org.herring.core.protocol.handler.MessageHandler;
+import org.herring.core.protocol.handler.SyncMessageHandler;
+
+import java.util.List;
 
 /**
  * << Description >>
@@ -12,39 +15,78 @@ import org.herring.core.protocol.handler.MessageHandler;
  * Time: 오전 11:21
  */
 public class NetworkFileSystemClient {
-    private String host;
-    private int port;
-
     private ClientComponent clientComponent;
-    private HerringCodec codec;
-    private MessageHandler messageHandler;
 
-    public NetworkFileSystemClient(String host, int port){
-        this.host = host;
-        this.port = port;
-
+    public NetworkFileSystemClient(String host, int port) {
         System.out.println("ClientComponent Initialize");
-        codec = new SerializableCodec();
-        messageHandler = new NetworkFileSystemMessageHandler();
-        clientComponent = new ClientComponent(this.host,this.port, codec,messageHandler);
+        HerringCodec codec = new SerializableCodec();
+        MessageHandler messageHandler = new SyncMessageHandler();
+        clientComponent = new ClientComponent(host, port, codec, messageHandler);
     }
 
     public void openConnection() throws Exception {
         clientComponent.start();
     }
 
-    public void closeConnection() throws Exception{
+    public void closeConnection() throws Exception {
         clientComponent.stop();
     }
 
-    public void putData(String locate, String data){
+    public void putData(String locate, String data) throws InterruptedException {
         //Command에 맞게 NetworkFileSystemAPIHandler 작성
         NetworkFileSystemAPIHandler apiHandler = new NetworkFileSystemAPIHandler();
-        apiHandler.makeCommand_putData_locate_data(locate,data);
+        apiHandler.makeCommand_putData_locate_data(locate, data);
 
-        //TODO: APIHandler를 Herring Codec으로 Encoding 하여 ClientComponent를 통해 Server로 전송해야 한다.
+        //APIHandler를 Herring Codec으로 Encoding 하여 ClientComponent를 통해 Server로 전송해야 한다.
         clientComponent.getNetworkContext().sendObject(apiHandler);
-        //TODO: 전송한 객체에 대한 서버에서의 처리 결과를 기다려야 한다.
+        //전송한 객체에 대한 서버에서의 처리 결과를 기다려야 한다.
+        clientComponent.getNetworkContext().waitUntil("received");
+        //서버에서 처리된 결과를 Queue에서 꺼내온다.
+        System.out.println("Processed on NFS : "+clientComponent.getNetworkContext().getMessageFromQueue());
     }
 
+    public void putData(String locate, List<String> dataList) throws InterruptedException {
+        NetworkFileSystemAPIHandler apiHandler = new NetworkFileSystemAPIHandler();
+        apiHandler.makeCommand_putData_locate_dataList(locate, dataList);
+
+        clientComponent.getNetworkContext().sendObject(apiHandler);
+        clientComponent.getNetworkContext().waitUntil("received");
+        System.out.println("Processed on NFS : "+clientComponent.getNetworkContext().getMessageFromQueue());
+    }
+
+    public String getData(String locate) throws InterruptedException {
+        NetworkFileSystemAPIHandler apiHandler = new NetworkFileSystemAPIHandler();
+        apiHandler.makeCommand_getData_locate(locate);
+
+        clientComponent.getNetworkContext().sendObject(apiHandler);
+        clientComponent.getNetworkContext().waitUntil("received");
+
+        String received = new String((byte[])clientComponent.getNetworkContext().getMessageFromQueue());
+        System.out.println("Processed result on NFS : "+received);
+        return received;
+    }
+
+    public String getData(String locate, int offset, int size) throws InterruptedException {
+        NetworkFileSystemAPIHandler apiHandler = new NetworkFileSystemAPIHandler();
+        apiHandler.makeCommand_getData_locate_offset_size(locate,offset,size);
+
+        clientComponent.getNetworkContext().sendObject(apiHandler);
+        clientComponent.getNetworkContext().waitUntil("received");
+
+        String received = new String((byte[])clientComponent.getNetworkContext().getMessageFromQueue());
+        System.out.println("Processed result on NFS : "+received);
+        return received;
+    }
+
+    public String getLine(String locate, int linecount) throws InterruptedException {
+        NetworkFileSystemAPIHandler apiHandler = new NetworkFileSystemAPIHandler();
+        apiHandler.makeCommand_getLine_locate_linecount(locate,linecount);
+
+        clientComponent.getNetworkContext().sendObject(apiHandler);
+        clientComponent.getNetworkContext().waitUntil("received");
+
+        String received = new String((byte[])clientComponent.getNetworkContext().getMessageFromQueue());
+        System.out.println("Processed result on NFS : "+received);
+        return received;
+    }
 }
