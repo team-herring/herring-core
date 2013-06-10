ad6483b7581c48b0ab43204482ed954eed21c220
HyunjeJo <retriever89@gmail.com>
2013-06-10 23:57:46 +0900

    NetworkFileSystemAPIHandler 주석 추가

--- src/main/java/org/herring/core/nfs/NetworkFileSystemAPIHandler.java
+++ src/main/java/org/herring/core/nfs/NetworkFileSystemAPIHandler.java
@@ -6,7 +6,9 @@ import org.junit.Assert;
 import java.util.List;
 
 /**
- * << Description >>
+ * NetworkFileSystemClient에서 사용한 API 종류와, 사용된 Parameter를 Packing 하는 클래스
+ * 객체를 생성한 후 makeCommand_ 로 시작하는 method 를 call 함으로써 사용된 api와 파라메터를 정의한다.
+ * <p/>
  * User: hyunje
  * Date: 13. 6. 10.
  * Time: 오후 1:35
@@ -30,9 +32,7 @@ public class NetworkFileSystemAPIHandler {
         this.linecount = -1;
     }
 
-
-
-    public void makeCommand_putData_locate_data(String locate, String data){
+    public void makeCommand_putData_locate_data(String locate, String data) {
         Assert.assertNotNull(locate);
         Assert.assertNotNull(data);
         this.command = APIs.putData_locate_data;
@@ -40,7 +40,7 @@ public class NetworkFileSystemAPIHandler {
         this.data = data;
     }
 
-    public void makeCommand_putData_locate_dataList(String locate, List<String> dataList){
+    public void makeCommand_putData_locate_dataList(String locate, List<String> dataList) {
         Assert.assertNotNull(locate);
         Assert.assertNotNull(dataList);
         this.command = APIs.putData_locate_datalist;
@@ -48,13 +48,13 @@ public class NetworkFileSystemAPIHandler {
         this.dataList = dataList;
     }
 
-    public void makeCommand_getData_locate(String locate){
+    public void makeCommand_getData_locate(String locate) {
         Assert.assertNotNull(locate);
         this.command = APIs.getData_locate;
         this.locate = locate;
     }
 
-    public void makeCommand_getData_locate_offset_size(String locate, int offset, int size){
+    public void makeCommand_getData_locate_offset_size(String locate, int offset, int size) {
         Assert.assertNotNull(locate);
         Assert.assertNotNull(offset);
         Assert.assertNotNull(size);
@@ -64,14 +64,13 @@ public class NetworkFileSystemAPIHandler {
         this.size = size;
     }
 
-    public void makeCommand_getLine_locate_linecount(String locate, int linecount){
+    public void makeCommand_getLine_locate_linecount(String locate, int linecount) {
         Assert.assertNotNull(locate);
         Assert.assertNotNull(linecount);
         this.command = APIs.getLine_locate_linecount;
         this.locate = locate;
         this.linecount = linecount;
     }
-
 
     public APIs getCommand() {
         return command;
@@ -108,6 +107,4 @@ public class NetworkFileSystemAPIHandler {
         getData_locate_offset_size,
         getLine_locate_linecount
     }
-
-
 }
